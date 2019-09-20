9919fd533684f261f6ab50cdc8c4f71667554f7e
yunxiang.gong <gyx_edu@qq.com>
2019-09-20 10:01:33 +0800

    Init for source

---
+++ src/ChainConfig.cpp
@@ -0,0 +1,23 @@
+/*
+ * ChainConfig.cpp
+ *
+ *  Created on: 2019年9月20日
+ *      Author: Gene Kong
+ */
+
+#include "ChainConfig.h"
+
+namespace FEmbed {
+
+ChainConfig::ChainConfig()
+{
+    // TODO Auto-generated constructor stub
+
+}
+
+ChainConfig::~ChainConfig()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace FEmbed */
---
+++ src/ChainConfig.h
@@ -0,0 +1,22 @@
+/*
+ * ChainConfig.h
+ *
+ *  Created on: 2019年9月20日
+ *      Author: Gene Kong
+ */
+
+#ifndef FEMBED_CHAINCONFIG_SRC_CHAINCONFIG_H_
+#define FEMBED_CHAINCONFIG_SRC_CHAINCONFIG_H_
+
+namespace FEmbed {
+
+class ChainConfig
+{
+ public:
+    ChainConfig();
+    virtual ~ChainConfig();
+};
+
+} /* namespace FEmbed */
+
+#endif /* FEMBED_CHAINCONFIG_SRC_CHAINCONFIG_H_ */
